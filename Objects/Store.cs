cf00243e18814a2eee004f074d2f26cbb341bfe8
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-03-04 09:04:08 -0800

    update store to know its name

--- Objects/Store.cs
+++ Objects/Store.cs
@@ -8,8 +8,22 @@ namespace BrandingNS.Objects
 {
   public class Store : DBHandler
   {
-    public Store()
+    private int _id;
+    private string _name;
+    public static string Table = "stores";
+    public static string NameColumn = "name";
+    
+    public Store(string name, int id = 0)
     {
+      _id = id;
+      _name = name;
     }
+    
+    public int GetId() { return _id; }
+    public string GetName() { return _name; }
+    public void SetName(string name) { _name = name; }
+    public void SetId(int id) { _id = id; }
+    
+    
   } // end class
 } // end namespace
