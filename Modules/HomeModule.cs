84dec5a6fb0f1243713062755cf5b1e463e28125
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-03-04 10:16:30 -0800

    Add brand routes to home module

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,7 +8,53 @@ namespace BrandingNS
     public HomeModule()
     {
       Get["/"] = _ => {
-        return View["header.cshtml"];
+        return View["index.cshtml"];
+      };
+      Get["/store/{sid}/"] = x => {
+        return View["viewStore.cshtml", Store.Find(int.Parse(x.sid))];
+      };
+      Get["/brand/{bid}/"] = x => {
+        return View["viewBrand.cshtml", Brand.Find(int.Parse(x.bid))];
+      };
+      Get["/store/{sid}/brands/"] = x => {
+        Dictionary<string, object> model = new Dictionary<string, object>{};
+        model.Add("store", Store.Find(int.Parse(x.sid)));
+        model.Add("brands", Brand.GetAll());
+        return View["viewStoreListBrands.cshtml", model];
+      };
+      Get["/brand/{bid}/stores/"] = x => {
+        Dictionary<string, object> model = new Dictionary<string, object>{};
+        model.Add("brand", Brand.Find(int.Parse(x.bid)));
+        model.Add("stores", Store.GetAll());
+        return View["viewBrandListStores.cshtml", model];
+      };
+      Get["/store/{sid}/addBrand/{bid}/"] = x => {
+        Store store = Store.Find(int.Parse(x.sid));
+        store.AddBrand(int.Parse(x.bid));
+        return View["forward.cshtml", "/store/"+x.sid];
+      };
+      Get["/brand/{bid}/addStore/{sid}/"] = x => {
+        Store store = Store.Find(int.Parse(x.sid));
+        store.AddBrand(int.Parse(x.bid));
+        return View["forward.cshtml", "/brand/"+x.bid];
+      };
+      Get["/store/{sid}/brand/{bid}/deleteRelationShip"] = x => {
+        StoreBrand.DeleteRelationship(int.Parse(x.sid), int.Parse(x.bid));
+        return View["forward.cshtml", "/store/"+x.sid];
+      };
+      Get["/brand/{bid}/store/{sid}/deleteRelationShip"] = x => {
+        StoreBrand.DeleteRelationship(int.Parse(x.sid), int.Parse(x.bid));
+        return View["forward.cshtml", "/brand/"+x.bid];
+      };
+      Post["/store/add/"] = _ => {
+        string name = Request.Form["name"];
+        new Store(name).Save();
+        return View["forward.cshtml", "/"];
+      };
+      Post["/brand/add/"] = _ => {
+        string name = Request.Form["name"];
+        new Brand(name).Save();
+        return View["forward.cshtml", "/"];
       };
     }
   }
