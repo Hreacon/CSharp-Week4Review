c612c9da0c815215542bd2a2b248f7fcdfd338a9
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-03-04 09:01:57 -0800

    Add test for store knows its own name

--- Tests/BrandingTest.cs
+++ Tests/BrandingTest.cs
@@ -9,13 +9,20 @@ namespace BrandingNS
 {
   public class StoreTest : IDisposable
   {
-     public StoreTest()
-     {
-       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=shoe_stores_test;Integrated Security=SSPI;";
-     }
-     public void Dispose()
-     {
-       Store.DeleteAll();
-     }
+    public StoreTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=shoe_stores_test;Integrated Security=SSPI;";
+    }
+    public void Dispose()
+    {
+      //  Store.DeleteAll();
+    }
+    
+    [Fact]
+    public void StoreKnowsItsName()
+    {
+      Store test = new Store("Name");
+      Assert.Equal("Name", test.GetName());
+    }
   }
 }
